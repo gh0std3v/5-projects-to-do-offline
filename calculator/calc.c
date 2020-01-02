2d0d7a2d0297d7e3996a6a961dbb0d539c9750ac
m4rtyr <ssharma10393@gmail.com>
2020-01-02 14:09:05 -0600

    Added handling for negative numbers and parentheses

--- calculator/calc.c
+++ calculator/calc.c
@@ -3,7 +3,7 @@
  * @Date:   2020-01-01T12:25:29-06:00
  * @Email:  silentcat@protonmail.com
  * @Last modified by:   silentcat
- * @Last modified time: 2020-01-01T18:39:34-06:00
+ * @Last modified time: 2020-01-02T14:08:27-06:00
  */
 
 /**
@@ -40,7 +40,7 @@ void stack_to_stack(STACK **aux, STACK **s);
 char *format_expression(char *expr);
 STACK *expr_to_stack(char *expr);
 double perform_op(double a, double b, char *token);
-STACK *eval_expr_parenth(STACK *s);
+STACK *eval_expr_parenth(STACK *s, STACK *tofree);
 double eval_expr(STACK *s);
 
 
@@ -94,8 +94,10 @@ char *format_expression(char *expr)
   int i = 0, k = 0;
   while (i < strlen(expr)) {
     if (expr[i] != ' ' && expr[i] != '\n' && expr[i] != '\t') {
-      if (isdigit(expr[i])) {
+      if (isdigit(expr[i]) || (expr[i] == '-' && isdigit(expr[i+1]) && (i == 0 || !isdigit(expr[i-1])))) {
         k = i;
+        if (expr[k] == '-')
+          strncat(nexpr, &expr[k++], 1);
         while (isdigit(expr[k]) || expr[k] == '.')
           strncat(nexpr, &expr[k++], 1);
         if (k < strlen(expr)-1)
@@ -147,28 +149,26 @@ double perform_op(double a, double b, char *token)
   return res;
 }
 
-STACK *eval_expr_parenth(STACK *s)
+STACK *eval_expr_parenth(STACK *s, STACK *tofree)
 {
-  STACK *aux = NULL, *tmp = NULL;
+  STACK *aux = NULL;
   while (s) {
     char *token = pop(&s);
-    if (!strcmp(token, "(")) {
-      token = pop(&s);
-      STACK *t = NULL;
-      while (strcmp(token, ")")) {
-        push(&t, token);
-        printf("%s\n", token);
-        token = pop(&s);
+    if (!strcmp(token, ")")) {
+      /* Pop off all tokens until we reach a '(' */
+      token = pop(&aux);
+      STACK *tmp = NULL;
+      while (strcmp(token, "(")) {
+        push(&tmp, token);
+        token = pop(&aux);
       }
-      while (t)
-        push(&tmp, pop(&t));
       double res = eval_expr(tmp);
       char *res_str = calloc(1, 20);
       snprintf(res_str, 20, "%e", res);
+      push(&tofree, res_str);
       push(&aux, res_str);
-    } else {
+    } else
       push(&aux, token);
-    }
   }
   return aux;
 }
@@ -190,7 +190,7 @@ double eval_expr(STACK *s)
            (type == EXP && !strcmp(token, "^"))) {
              op_found = 1;
              push(&aux, token);
-      } else if (op_found && isdigit(token[0])) {
+      } else if (op_found && (isdigit(token[0]) || token[0] == '-')) {
         char *operator = pop(&aux), *operand = pop(&aux);
         double op2 = strtod(token, NULL), op1 = strtod(operand, NULL);
         double res = perform_op(op1, op2, operator);
@@ -209,12 +209,22 @@ double eval_expr(STACK *s)
 int main(int argc, char *argv[])
 {
   char buffer[MAX_LINE+1], *expr;
+  printf("Type 'quit' to exit\n");
   while (1) {
     printf("> ");
     fgets(buffer, MAX_LINE+1, stdin);
+    if (!strcmp(buffer, "quit\n"))
+      break;
     expr = format_expression(buffer);
     STACK *s = expr_to_stack(expr);
-    printf("%f\n", eval_expr(s));
+    STACK *tmp = NULL, *tofree = NULL;
+    s = eval_expr_parenth(s, tofree);
+    stack_to_stack(&s, &tmp);
+    printf("%f\n", eval_expr(tmp));
+    while (tofree) {
+      char *value = pop(&tofree);
+      free(value);
+    }
     free(expr);
   }
   return 0;
