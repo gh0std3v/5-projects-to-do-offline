c4153c6d4c44a8e961ab926754b3e64296d9619d
m4rtyr <ssharma10393@gmail.com>
2020-01-04 18:06:40 -0600

    Added highlighting and multiple files to simple_grep

--- grep/simple_grep.c
+++ grep/simple_grep.c
@@ -3,7 +3,7 @@
  * @Date:   2020-01-04T16:58:18-06:00
  * @Email:  silentcat@protonmail.com
  * @Last modified by:   silentcat
- * @Last modified time: 2020-01-04T17:36:36-06:00
+ * @Last modified time: 2020-01-04T18:06:36-06:00
  */
 
 /**
@@ -24,7 +24,7 @@
 char *read_file(const char *path);
 void read_tokens(char *buffer, char *match);
 int is_match(char *token, char *match);
-void write_line(char *token, char *line, int n);
+void write_line(char *token, char *line, int n, int pos);
 
 
 char *read_file(const char *path)
@@ -61,12 +61,14 @@ void read_tokens(char *buffer, char *match)
   int n = 1;
   char *cpy = strdup(buffer), *line, *token;
   while ((line = strsep(&cpy, "\n"))) {
+    int pos = 0;
     char *line_cpy = strdup(line);
     while ((token = strsep(&line_cpy, " "))) {
       if (is_match(token, match)) {
-        write_line(token, line, n);
+        write_line(token, line, n, pos);
         break;
       }
+      pos += strlen(token) + 1;
     }
     while ((token = strsep(&line_cpy, " ")));
     free(line_cpy);
@@ -80,9 +82,20 @@ int is_match(char *token, char *match)
   return !strncmp(match, token, strlen(match));
 }
 
-void write_line(char *token, char *line, int n)
+void write_line(char *token, char *line, int n, int pos)
 {
-  printf("%d: %s\n", n, line);
+  char *str = calloc(1, 2*strlen(line)+1);
+  if (!str) {
+    print_err();
+    return;
+  }
+  strncpy(str, line, pos);
+  strncat(str, "\033[0;31m", 7); // Red
+  strncat(str, line + pos, strlen(token));
+  strncat(str, "\033[0m", 5); // No color
+  strncat(str, line + pos + strlen(token), strlen(line)-strlen(token)-pos);
+  printf("%d: %s\n", n, str);
+  free(str);
 }
 
 int main(int argc, char *argv[])
@@ -91,8 +104,14 @@ int main(int argc, char *argv[])
     fprintf(stderr, "Usage: %s [match] [filename]\n", argv[0]);
     return 1;
   }
-  char *buffer = read_file(argv[2]);
-  read_tokens(buffer, argv[1]);
-  free(buffer);
+
+  for (int i = 2; i < argc; i++) {
+    printf("\033[1;33m%s\033[0m \n", argv[i]); // Yellow, string, no color 
+    char *buffer = read_file(argv[i]);
+    if (buffer) {
+      read_tokens(buffer, argv[1]);
+      free(buffer);
+    }
+  }
   return 0;
 }
